af84ea63660720f817964b6ab9d51720ce9bce95
Felicia Karlsson <felizia02@icloud.com>
2025-02-28 09:14:11 +0100

    spring boot project started

---
+++ idea-to-mvp/src/main/java/Idea/To/MVP/IdeaToMvpApplication.java
@@ -0,0 +1,13 @@
+package Idea.To.MVP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaToMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaToMvpApplication.class, args);
+	}
+
+}
---
+++ idea-to-mvp/src/test/java/Idea/To/MVP/IdeaToMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package Idea.To.MVP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaToMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
