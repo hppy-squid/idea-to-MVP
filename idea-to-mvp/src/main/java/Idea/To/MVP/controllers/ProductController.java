a856edae4c27bfbb6ee182af1f3b948670b1a478
Elliott <sendtoelle@hotmail.com>
2025-03-04 11:16:32 +0100

    test-checkout utan frontend funkar

--- idea-to-mvp/src/main/java/Idea/To/MVP/controllers/ProductController.java
+++ idea-to-mvp/src/main/java/Idea/To/MVP/controllers/ProductController.java
@@ -1,23 +1,37 @@
 package Idea.To.MVP.controllers;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import Idea.To.MVP.models.Product;
+import Idea.To.MVP.service.ProductService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class ProductController {
-    
+
+    @Autowired
+    ProductService productService;
+
+    @PostMapping("/test-checkout")
+    public Map<String, String> createCheckoutSession() {
+
+        return productService.createCheckoutSession();
+    }
+
     @GetMapping("/testProduct")
     public ResponseEntity<Product> getTestProduct() {
 
         Product product = new Product();
         product.setName("test");
 
-        return ResponseEntity.ok(product); 
+        return ResponseEntity.ok(product);
     }
-    
+
 }
--- idea-to-mvp/src/main/java/Idea/To/MVP/security/SecurityConfig.java
+++ idea-to-mvp/src/main/java/Idea/To/MVP/security/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests
                         .anyRequest().permitAll())
                 .formLogin(Customizer.withDefaults())
