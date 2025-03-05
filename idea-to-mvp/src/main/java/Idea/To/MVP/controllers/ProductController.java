c2b0a4c204a6d1501689d49252e07b98abc8552a
Alice Fearn <alice.fearn@hotmail.com>
2025-03-05 12:08:10 +0100

    la till add, gett och delete produkt

--- idea-to-mvp/src/main/java/Idea/To/MVP/controllers/ProductController.java
+++ idea-to-mvp/src/main/java/Idea/To/MVP/controllers/ProductController.java
@@ -1,16 +1,24 @@
 package Idea.To.MVP.controllers;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import Idea.To.MVP.models.Product;
 import Idea.To.MVP.service.ProductService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -19,19 +27,43 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    @PostMapping("/test-checkout")
-    public Map<String, String> createCheckoutSession() {
+    // @PostMapping("/test-checkout")
+    // public Map<String, String> createCheckoutSession() {
 
-        return productService.createCheckoutSession();
+    //     return productService.createCheckoutSession();
+    // }
+
+    // @GetMapping("/testProduct")
+    // public ResponseEntity<Product> getTestProduct() {
+
+    //     Product product = new Product();
+    //     product.setName("test");
+
+    //     return ResponseEntity.ok(product);
+    // }
+
+    @PostMapping("/product")
+    public void addProduct(@RequestBody Product product) {
+        productService.addProduct(product);
     }
 
-    @GetMapping("/testProduct")
-    public ResponseEntity<Product> getTestProduct() {
-
-        Product product = new Product();
-        product.setName("test");
-
-        return ResponseEntity.ok(product);
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
     }
 
+    @GetMapping("/product") 
+    public Optional<Product> getProductById(@RequestParam("id") UUID id) {
+       return productService.getProductById(id);
+    }
+
+    @DeleteMapping("/product")
+    public String deleteProductById(@RequestParam("id") UUID id) {
+        return productService.deleteProductById(id);
+    }
+
+    // @PatchMapping("/product/{id}")
+    // public void patchProductById(@PathVariable("id") UUID id) {
+    //     productService.patchProductById();
+    // }
 }
