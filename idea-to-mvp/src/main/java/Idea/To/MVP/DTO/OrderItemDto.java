caabbd8e98201ff01ae78776cdd82d4d929ca819
Azadan <azadan@live.com>
2025-03-05 22:10:32 +0100

    Add attributes to OrderItemDto

--- idea-to-mvp/src/main/java/Idea/To/MVP/DTO/OrderItemDto.java
+++ idea-to-mvp/src/main/java/Idea/To/MVP/DTO/OrderItemDto.java
@@ -1,4 +1,13 @@
 package Idea.To.MVP.DTO;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
 public class OrderItemDto {
+    private UUID id;
+    private Long antal;
+    private BigDecimal price;
 }
